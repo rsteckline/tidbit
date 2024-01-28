24a403d8bd554f04963604b81027eaad8208cce9
Bobby Steckline <rjsturing@gmail.com>
2024-01-27 21:35:19 -0500

    Add working search function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,23 @@
-import './App.css'
+import React, { useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../Homepage/Homepage";
 import Header from '../Header/Header';
 import ArticleDetails from '../ArticleDetails/ArticleDetails';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path='/article' element={<ArticleDetails />} />
-    </Routes>
+      <Header onSearchChange={handleSearchChange} />
+      <Routes>
+        <Route path="/" element={<Homepage searchQuery={searchQuery}/>} />
+        <Route path='/article' element={<ArticleDetails />} />
+      </Routes>
     </>
   );
 }
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from 'react';
-import getNews  from '../../apiCalls';
+import getNews from '../../apiCalls';
 import './Homepage.css';
 import HomepageList from '../HomepageList/HomepageList';
-import { Link } from 'react-router-dom';
 
-export default function Homepage() {
+export default function Homepage({ searchQuery }) {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
     if (!articles.length) {
-        getNews().then(data => setArticles(data.articles));
-      }
+      getNews().then(data => setArticles(data.articles));
+    }
   }, []);
+
+  const filteredArticles = articles.filter(article => {
+    const query = searchQuery.toLowerCase();
+    const titleMatches = article.title.toLowerCase().includes(query);
+    const descriptionMatches = article.description && article.description.toLowerCase().includes(query);
+    return titleMatches || descriptionMatches;
+  });
 
   return (
     <div className='page'>
       <main>
         <h2 className='h2'>Latest Top Stories</h2>
-        <HomepageList articles={articles}/>
+        <HomepageList articles={filteredArticles}/>
       </main>
     </div>
   );
